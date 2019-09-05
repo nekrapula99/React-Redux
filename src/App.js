77999b9b374fccf88a48794dd0f82bc9d23bdb5b
Osorio <no185020@ncr.com>
2019-09-05 13:24:32 -0500

    Commit from NCR office

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+  /*   return (
       <div className="App">
         <h1>Im React App</h1>  
       </div>
-    );
+    ); */
+    return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Does will work?'))
   }
 }
 
